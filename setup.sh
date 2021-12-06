1745aa4725abbe8370127b0f6fe713aabadda1bb
haokai-li <82911890+haokai-li@users.noreply.github.com>
2021-12-06 12:00:21 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# you will need to change permissions on the file before you can run it
+# chmod +x ./load.sh
+
+# then at the end, reboot for everything to take effect
+# sudo reboot now
+
+
+
+# update and upgrade system
+echo Update and upgrade system
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
