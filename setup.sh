b3f5e1fd547155299bcd6ba4e053ba8ad5f40660
haokai-li <82911890+haokai-li@users.noreply.github.com>
2021-12-06 12:04:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,14 @@
 echo Update and upgrade system
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
+
+# load .bashrc file
+echo Load .bashrc file
+> ~/.bashrc
+cp ./.bashrc./.bashrc ~/.bashrc
+# dircolors -b >> .bashrc
+sudo chown ubuntu ~/.bash_profile
+echo '' >> ~/.bash_profile
+echo 'if [ -f $HOME/.bashrc ]; then' >> ~/.bash_profile
+echo '    source $HOME/.bashrc' >> ~/.bash_profile
+echo 'fi' >> ~/.bash_profile
